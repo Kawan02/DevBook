4b6b434db833ad5ea35880ba023c75e480232652
Kawan Messias <kawan.messias@trescon.com.br>
2023-11-16 12:29:05 -0300

    install jquery e configurando a rota de cadastro

---
+++ web-app/assets/js/cadastro.js
@@ -0,0 +1,28 @@
+$('#formulario-cadastro').on('submit', criarUsuario)
+
+function criarUsuario(evento) {
+    evento.preventDefault();
+    console.log("Testando...")
+
+
+    if ($('#senha').val() != $('#confirmar-senha').val()) {
+        alert("As senhas não coincidem!")
+        return
+    }
+
+    $.ajax({
+        url: "/usuarios",
+        method: "POST",
+        data: {
+            nome: $('#nome').val(),
+            nick: $('#nick').val(),
+            email: $('#email').val(),
+            senha: $('#senha').val(),
+        }
+    }).done(function () { // 200, 201, 204
+        alert("Usuário cadastrado com sucesso!");
+    }).fail(function (erro) { // 400, 404, 401, 403, 500
+        console.log(erro);
+        alert("Erro ao cadastrar o usuário!");
+    });
+}
