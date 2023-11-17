f5c34d989ce70930c1fff96ae15f96672966d3ca
Kawan Messias <kawan.messias@trescon.com.br>
2023-11-17 13:49:28 -0300

    Autenticação

---
+++ web-app/src/controllers/middlerawes/middlerawes.go
@@ -0,0 +1,25 @@
+package middlerawes
+
+import (
+	"log"
+	"net/http"
+	"webapp/src/cookies"
+)
+
+// Logger escreve informações da requisição no terminal
+func Logger(proximaFuncao http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("\n %s %s %s", r.Method, r.RequestURI, r.Host)
+		proximaFuncao(w, r)
+	}
+}
+
+func Autenticar(proximaFuncao http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if _, erro := cookies.Ler(r); erro != nil {
+			http.Redirect(w, r, "/login", http.StatusFound)
+			return
+		}
+		proximaFuncao(w, r)
+	}
+}
