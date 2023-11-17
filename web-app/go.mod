86770cfa3206422e066a304c0f1227ca15dd0214
Kawan Messias <kawan.messias@trescon.com.br>
2023-11-17 11:38:57 -0300

    Pacote config, variaveis de ambiente

--- web-app/go.mod
+++ web-app/go.mod
@@ -2,4 +2,7 @@ module webapp
 
 go 1.21.3
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
+)
--- web-app/main.go
+++ web-app/main.go
@@ -4,14 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"webapp/src/config"
 	"webapp/src/router"
 	"webapp/src/utils"
 )
 
 func main() {
+	config.Carregar()
 	utils.CarregarTemplates()
 	rotas := router.Gerar()
 
-	fmt.Println("Rodando o web-app")
-	log.Fatal(http.ListenAndServe(":3000", rotas))
+	fmt.Println(config.HashKey)
+
+	fmt.Printf("Escutando na porta: %d\n", config.Porta)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), rotas))
 }
