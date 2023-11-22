bde47bc4aad7eb7101f40abcae2d0e1b88053bd8
Kawan Messias <kawan.messias@trescon.com.br>
2023-11-22 10:43:19 -0300

    Estilizando a pagina principal

--- web-app/src/models/Publicacoes.go
+++ web-app/src/models/Publicacoes.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type Publicacao struct {
-	ID         uint64    `json:"id,omitempty"`
-	Titulo     string    `json:"titulo,omitempty"`
-	Conteudo   string    `json:"conteudo,omitempty"`
-	AuthorID   uint64    `json:"authorId,omitempty"`
-	AuthorNick uint64    `json:"authorNick,omitempty"`
-	Curtidas   uint64    `json:"curtidas"`
-	CriadaEm   time.Time `json:"criadaEm,omitempty"`
+	ID        uint64    `json:"id,omitempty"`
+	Titulo    string    `json:"titulo,omitempty"`
+	Conteudo  string    `json:"conteudo,omitempty"`
+	AuthorID  uint64    `json:"authorId,omitempty"`
+	AutorNick string    `json:"autorNick,omitempty"`
+	Curtidas  uint64    `json:"curtidas"`
+	CriadaEm  time.Time `json:"criadaEm,omitempty"`
 }
