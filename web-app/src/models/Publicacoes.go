2cd20a0e963b9bafd05d572d4a299cd71788ffc9
Kawan Messias <kawan.messias@trescon.com.br>
2023-11-22 14:30:55 -0300

    Implementação do método de criar publicações

--- web-app/src/models/Publicacoes.go
+++ web-app/src/models/Publicacoes.go
@@ -6,7 +6,7 @@ type Publicacao struct {
 	ID        uint64    `json:"id,omitempty"`
 	Titulo    string    `json:"titulo,omitempty"`
 	Conteudo  string    `json:"conteudo,omitempty"`
-	AuthorID  uint64    `json:"authorId,omitempty"`
+	AutorID   uint64    `json:"autorId,omitempty"`
 	AutorNick string    `json:"autorNick,omitempty"`
 	Curtidas  uint64    `json:"curtidas"`
 	CriadaEm  time.Time `json:"criadaEm,omitempty"`
