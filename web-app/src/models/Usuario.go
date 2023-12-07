65439aec0255747233d8f67e45c046b4ebac1c2d
Kawan Messias <kawan.messias@trescon.com.br>
2023-12-07 14:31:22 -0300

    Página do perfil do usuário

--- web-app/src/models/Usuario.go
+++ web-app/src/models/Usuario.go
@@ -1,6 +1,9 @@
 package models
 
-import "time"
+import (
+	"net/http"
+	"time"
+)
 
 type Usuario struct {
 	ID          uint64       `json:"id"`
@@ -11,4 +14,34 @@ type Usuario struct {
 	Seguidores  []Usuario    `json:"seguidores"`
 	Seguindo    []Usuario    `json:"seguindo"`
 	Publicacoes []Publicacao `json:"publicacoes"`
+}
+
+// BuscarUsuarioCompleto faz 4 requisições na API para montar o usuário
+func BuscarUsuarioCompleto(usuarioID uint64, r *http.Request) (Usuario, error) {
+	canalUsuario := make(chan Usuario)
+	canalSeguidores := make(chan []Usuario)
+	canalSeguindo := make(chan []Usuario)
+	canalPublicacoes := make(chan []Publicacao)
+
+	go BuscarDadosDoUsuario(canalUsuario, usuarioID, r)
+	go BuscarSeguidores(canalSeguidores, usuarioID, r)
+	go BuscarSeguindo(canalSeguindo, usuarioID, r)
+	go BuscarPublicacoes(canalPublicacoes, usuarioID, r)
+
+}
+
+func BuscarDadosDoUsuario(canal <-chan Usuario, usuarioID uint64, r *http.Request) {
+
+}
+
+func BuscarSeguidores(canal <-chan []Usuario, usuarioID uint64, r *http.Request) {
+
+}
+
+func BuscarSeguindo(canal <-chan []Usuario, usuarioID uint64, r *http.Request) {
+
+}
+
+func BuscarPublicacoes(canal <-chan []Publicacao, usuarioID uint64, r *http.Request) {
+
 }
